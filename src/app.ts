fbfeb0c75db9deafae3cac4326bc8e3b9d3913b4
Lucas Bryan <lucasbryan0217@gmail.com>
2022-05-18 12:18:12 -0300

    import routes and implement app

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,9 @@
 import express, { NextFunction, Request, Response } from 'express';
-// import routes from "./routes"
+import routes from './routes';
 const app = express();
 
 app.use(express.json());
-// app.use(routes)
+app.use(routes);
 
 // app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
 //   if (err instanceof AppError) {
