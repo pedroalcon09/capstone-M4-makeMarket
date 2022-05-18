d0f24c4dead3633bdf18bf69eda9f300dbf35e70
Lucas Bryan <lucasbryan0217@gmail.com>
2022-05-18 12:43:01 -0300

    create a base exported index from interfaces

---
+++ src/interfaces/index.ts
@@ -0,0 +1,6 @@
+export * from './buyer.interfaces';
+export * from './buys.interfaces';
+export * from './categories.interfaces';
+export * from './favourites.interfaces';
+export * from './products.interface';
+export * from './seller.interfaces';
