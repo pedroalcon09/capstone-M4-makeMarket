1ea8c12d9acc8527f888c5a06032636869bb41a3
pedroalcon09 <pedroalcong@hotmail.com>
2022-05-19 16:59:27 -0300

    fix: alteração do tipo do erro do Error.middleware para any

--- src/middlewares/Error.middleware.ts
+++ src/middlewares/Error.middleware.ts
@@ -1,16 +1,21 @@
 import { NextFunction, Request, Response } from "express";
-import { AppError } from './errors/appError'
+import { AppError } from "./errors/appError";
 
-export const errorMiddleware = (err: Error, request: Request, response: Response, _: NextFunction) => {
-    if (err instanceof AppError) {
-        return response.status(err.statusCode).json({
-          status: "error",
-          message: err.message,
-        });
-      }
-      console.error(err);
-      return response.status(500).json({
-        status: "error",
-        message: "Internal server error",
-      });
-}
+export const errorMiddleware = (
+  err: any,
+  request: Request,
+  response: Response,
+  _: NextFunction
+) => {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({
+      status: "error",
+      message: err.message,
+    });
+  }
+  console.error(err);
+  return response.status(500).json({
+    status: "error",
+    message: "Internal server error",
+  });
+};
