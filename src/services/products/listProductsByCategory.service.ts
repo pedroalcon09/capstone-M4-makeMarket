717dc2714aced9d923971ffcbb2c4adab625a57d
Junior da Silva <jr.a.silva@outlook.com>
2022-05-20 08:12:23 -0300

    build: list products by category

---
+++ src/services/products/listProductsByCategory.service.ts
@@ -0,0 +1,21 @@
+import { AppDataSource } from "../../data-source";
+import { AppError } from "../../errors/appError";
+import { Product } from "../../entities/product.entity";
+
+const listProductsService = async (category_id: string) => {
+  const productRepository = AppDataSource.getRepository(Product);
+
+  const products = await productRepository.find();
+
+  const productByCategory = products.find(
+    (product) => product.category_id === category_id
+  );
+
+  if (!productByCategory) {
+    throw new AppError(404, "No products in this category");
+  }
+
+  return productByCategory;
+};
+
+export default listProductsService;
