3b2f7eda38e5fcc140ea76f7be420c8103d6c534
pedroalcon09 <pedroalcong@hotmail.com>
2022-05-20 15:23:21 -0300

    salvando progresso dos controllers

--- src/controllers/buyerController.controller.ts
+++ src/controllers/buyerController.controller.ts
@@ -7,10 +7,19 @@ import listBuyersService from "../service/Buyer/listBuyers.service";
 import listBuyerByIdService from "../service/Buyer/listBuyerById.service";
 import updateBuyerService from "../service/Buyer/updateBuyer.service";
 import deleteBuyerService from "../service/Buyer/deleteBuyer.service";
+import { IBuyerLogin } from "../interfaces";
 
 export default class BuyerController {
   static async login(req: Request, res: Response) {
     try {
+      const { email, password } = req.body;
+
+      const token = buyerLoginService(email, password);
+
+      return res.status(201).json({
+        message: "Logged in!",
+        token,
+      });
     } catch (err) {
       if (err instanceof AppError) {
         handleError(err, res);
