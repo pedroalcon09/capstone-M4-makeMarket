615cd578b2e62b1fd1fce22bc6414447087bc141
pedroalcon09 <pedroalcong@hotmail.com>
2022-05-20 15:13:49 -0300

    salvando progresso dos controllers

--- src/controllers/buyerController.controller.ts
+++ src/controllers/buyerController.controller.ts
@@ -1,6 +1,13 @@
 import { Request, Response } from "express";
 import { AppError, handleError } from "../errors/appError";
 
+import buyerLoginService from "../service/Buyer/buyerLogin.service";
+import createBuyerService from "../service/Buyer/createBuyer.service";
+import listBuyersService from "../service/Buyer/listBuyers.service";
+import listBuyerByIdService from "../service/Buyer/listBuyerById.service";
+import updateBuyerService from "../service/Buyer/updateBuyer.service";
+import deleteBuyerService from "../service/Buyer/deleteBuyer.service";
+
 export default class BuyerController {
   static async login(req: Request, res: Response) {
     try {
