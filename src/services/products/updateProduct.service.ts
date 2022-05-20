d2dedd9642ecccb207918360af68f6e3a42c1bc5
Junior da Silva <jr.a.silva@outlook.com>
2022-05-20 08:45:34 -0300

    build: update product

---
+++ src/services/products/updateProduct.service.ts
@@ -0,0 +1,33 @@
+import { IProductsUpdate } from "../../interfaces/index.ts";
+import { AppDataSource } from "../../data-source";
+import { AppError } from "../../errors/appError";
+import { Product } from "../../entities/product.entity";
+
+const updateProductService = async (
+  product_id: string,
+  updateData: IProductsUpdate
+) => {
+  const productRepository = AppDataSource.getRepository(Product);
+
+  const products = await productRepository.find();
+
+  const productToBeUpdated = products.find(
+    (product) => product.product_id === product_id
+  );
+
+  if (!productToBeUpdated) {
+    throw new AppError(404, "No product with this id");
+  }
+
+  await productRepository.update(productToBeUpdated!.id, {
+    name: updateData.name,
+    price: updateData.price,
+    description: updateData.description,
+    stock: updateData.stock,
+    updated_at: new Date(),
+  });
+
+  return true;
+};
+
+export default updateProductService;
