99cec266af35bd794d7150375322b0a88ab9ff3c
Junior da Silva <jr.a.silva@outlook.com>
2022-05-20 04:35:56 -0300

    build: create product

--- src/controllers/productsController.controller.ts
+++ src/controllers/productsController.controller.ts
@@ -1,9 +1,25 @@
 import { Request, Response } from "express";
 import { AppError, handleError } from "../errors/appError";
+import createProductService from "../../services/products/createProduct.service.ts";
 
 export default class ProductsController {
   static async create(req: Request, res: Response) {
     try {
+      const { name, price, description, stock, url_img, category_id } =
+        req.body;
+      const { sellerId } = req.params;
+
+      const newProduct = await createProductService({
+        name,
+        price,
+        description,
+        stock,
+        url_img,
+        category_id,
+        sellerId,
+      });
+
+      return res.status(201).json({ newProduct });
     } catch (err) {
       if (err instanceof AppError) {
         handleError(err, res);
