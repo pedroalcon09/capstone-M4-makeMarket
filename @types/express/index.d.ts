5b2b298d6549d470bf47b9e4817aadabbe4e8dc9
pedroalcon09 <pedroalcong@hotmail.com>
2022-05-21 14:45:45 -0300

    feat: Criação do schemas de validação de criação e atualização do comprador

--- @types/express/index.d.ts
+++ @types/express/index.d.ts
@@ -1,9 +1,15 @@
-import * as express from "express"
+import * as express from "express";
+import {
+  IBuyerCreate,
+  IBuyerUpdate,
+} from "../../src/interfaces/buyer.interfaces";
 
-declare global{
-    namespace Express{
-        interface Request {
-            userId: string
-        }
+declare global {
+  namespace Express {
+    interface Request {
+      userId: string;
+      newBuyer: IBuyerCreate;
+      updateBuyer: IBuyerUpdate;
     }
+  }
 }
---
+++ src/middlewares/Buyer/validateBuyerUpdate.middleware.ts
@@ -0,0 +1,46 @@
+import { IBuyerUpdate } from "../../interfaces";
+import { Request, Response, NextFunction } from "express";
+
+import * as yup from "yup";
+
+//Importar a type de Schemas
+import { SchemaOf } from "yup";
+
+const pwdRegex: RegExp = /(?=.*[!@#$&*])(?=.*[0-9])(?=.{6,})/g;
+
+//Criar o schema com ShcemaOf e a Interface do schema
+export const buyerUpdateSchema: SchemaOf<IBuyerUpdate> = yup.object().shape({
+  name: yup.string(),
+  email: yup.string().email("Wrong email format"),
+  password: yup
+    .string()
+    .matches(
+      pwdRegex,
+      "Password must be 6 char long, with number and special char(!,@,#,$,&,*)"
+    ),
+});
+
+//Criar função pra fazer a validação
+export const validateBuyerUpdate = (schema: SchemaOf<IBuyerUpdate>) => {
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const data = req.body;
+
+      try {
+        const validateData = await schema.validate(data, {
+          abortEarly: false,
+          stripUnknown: true,
+        });
+        req.updateBuyer = validateData;
+
+        next();
+      } catch (err: any) {
+        const error = err.errors?.join(", ");
+
+        return res.status(400).json({ error });
+      }
+    } catch (err) {
+      next(err);
+    }
+  };
+};
