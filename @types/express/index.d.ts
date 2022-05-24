2d7d235bceba35f6b0a6815ee56fbaa52ae06eba
Rafael Ronconi Dona <rafael.ronconi@hotmail.com>
2022-05-24 15:38:22 -0300

    feat: Criação do schema de criação de categoria

--- @types/express/index.d.ts
+++ @types/express/index.d.ts
@@ -1,9 +1,11 @@
 import * as express from "express";
+import { ICategories } from "../../src/interfaces/categories.interfaces";
 
 declare global {
   namespace Express {
     interface Request {
       userId: string;
+      newCategory: ICategoryCreate
     }
   }
 }
