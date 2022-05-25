505879f706ab5a903ccd83b1c987bc9866616242
pedroalcon09 <pedroalcong@hotmail.com>
2022-05-25 15:17:41 -0300

    fix: debug app

--- src/routes/buyer.routes.ts
+++ src/routes/buyer.routes.ts
@@ -45,3 +45,17 @@ buyerRoutes.delete(
 ); // -- OK
 
 export default buyerRoutes;
+
+/* 
+import { Router } from "express";
+
+import favoriteController from "../controllers/favoriteController.controller";
+
+const favouritesRoutes = Router();
+
+favouritesRoutes.post("/:buyerId/:productId", favoriteController.create);
+favouritesRoutes.get("/:buyerId", favoriteController.list);
+favouritesRoutes.delete("/:buyerId/:productId", favoriteController.delete);
+
+export default favouritesRoutes;
+ */
