3d35ae876897ef8886f64287a89ab18415bcdc08
pedroalcon09 <pedroalcong@hotmail.com>
2022-05-25 15:27:47 -0300

    fix: debugando rotas e alterando funcionamento do favourites

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,18 +1,16 @@
-import { Router } from 'express';
-import buyerRoutes from './buyer.routes';
-import buysRoutes from './buys.routes';
-import categoriesRoutes from './categories.routes';
-import favouritesRoutes from './favourites.routes';
-import productsRoutes from './products.routes';
-import sellerRouter from './seller.routes';
+import { Router } from "express";
+import buyerRoutes from "./buyer.routes";
+import buysRoutes from "./buys.routes";
+import categoriesRoutes from "./categories.routes";
+import productsRoutes from "./products.routes";
+import sellerRouter from "./seller.routes";
 
 const routes = Router();
 
-routes.use('/seller', sellerRouter);
-routes.use('/buyer', buyerRoutes);
-routes.use('/products', productsRoutes);
-routes.use('/categories', categoriesRoutes);
-routes.use('/favourites', favouritesRoutes);
-routes.use('/buy', buysRoutes);
+routes.use("/seller", sellerRouter);
+routes.use("/buyer", buyerRoutes);
+routes.use("/products", productsRoutes);
+routes.use("/categories", categoriesRoutes);
+routes.use("/buy", buysRoutes);
 
 export default routes;
